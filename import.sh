11233b9ee1b830bec9985cc89fe3be2cf173434d
Pavel Chuchuva <pavel@chuchuva.com>
2024-05-11 09:05:25 +1000

    Pass path to web log as argument

--- import.sh
+++ import.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $# -eq 0 ]] ; then
+  echo 'Pass path to web-access.log as argument, for example bash import.sh web-access.log'
+  exit 1
+fi
+
 if test -f lines-imported.txt; then
   lines_imported=$(<lines-imported.txt)
 else
@@ -7,7 +12,7 @@ else
 fi
 
 echo "Previously imported: $lines_imported"
-tail -n +"$((lines_imported+1))" web-access.log > new.log || exit 1
+tail -n +"$((lines_imported+1))" $1 > new.log || exit 1
 node import.js || exit 1
 new_lines_imported=$(wc -l < new.log)
 echo "new_lines_imported: $new_lines_imported"
